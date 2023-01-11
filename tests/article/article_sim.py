2a1b800de1dc2ab244f0e47d517f9e78d95801da
Trym Tengesdal <tengesdal1994@gmail.com>
2023-01-11 15:06:37 +0100

    Add article sim file

---
+++ tests/article/article_sim.py
@@ -0,0 +1,25 @@
+from colav_evaluation_tool.evaluator import Evaluator
+from colav_simulator.simulator import Simulator
+
+if __name__ == "__main__":
+
+    simulator = Simulator()
+
+    output = simulator.run()
+    vessels_data_list = output["vessels_data_list"]
+
+    evaluator = Evaluator()
+
+    count = 0
+    for vessels in vessels_data_list:
+        print("Evaluating scenario " + str(count) + " with " + str(len(vessels)) + " vessels...")
+        evaluator.set_vessel_data(vessels)
+        evaluator.evaluate()
+
+        evaluator.print_vessel_scores(vessel_id=0)
+        # evaluator.plot_trajectories_and_scores(0, [0, 1])
+        # evaluator.plot_scores(vessel_ids=[0, 1])
+        # evaluator.plot_maneuver_detection_information(vessel_id=0)
+
+        count += 1
+    print("done")
