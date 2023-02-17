87ab5fd6ae34580f5f89ab5873cf8bbf2e6fcacb
Trym Tengesdal <tengesdal1994@gmail.com>
2023-02-17 15:52:55 +0100

    Fix maxlength on csog state. Fix some scenarios. Fix interface between simulator and evaluator

--- tests/test_simulation_and_evaluation.py
+++ tests/test_simulation_and_evaluation.py
@@ -2,21 +2,23 @@ from colav_evaluation_tool.evaluator import Evaluator
 from colav_simulator.simulator import Simulator
 
 if __name__ == "__main__":
+    # ScenaroGenerator.
 
     simulator = Simulator()
 
     output = simulator.run()
-    vessels_data_list = output["vessels_data_list"]
-    scenario_config_list = output["scenario_config_list"]
-    scenario_enc_list = output["scenario_enc_list"]
+    episode_simdata_list = output[0]["episode_simdata_list"]
+    episode = episode_simdata_list[0]
+    enc = output[0]["enc"]
 
     evaluator = Evaluator()
 
     count = 0
-    for vessels in vessels_data_list:
+    for episode_data in episode_simdata_list:
+        vessels = episode_data["vessel_data"]
         print("Evaluating scenario " + str(count) + " with " + str(len(vessels)) + " vessels...")
 
-        evaluator.set_enc(scenario_enc_list[count])
+        evaluator.set_enc(enc)
         evaluator.set_vessel_data(vessels)
         results = evaluator.evaluate()
 
