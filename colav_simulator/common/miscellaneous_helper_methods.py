Fix bug in course computation function. Add ground truth tracking observation.

colav_simulator/common/miscellaneous_helper_methods.py
@@ -851,13 +851,13 @@ def convert_3dof_state_to_sog_cog_state(xs: np.ndarray) -> np.ndarray:
     """
     if xs.ndim == 1:
         heading = xs[2]
-        crab_angle = 0.0  # np.arctan2(xs[4], xs[3])
+        crab_angle = np.arctan2(xs[4], xs[3])
         cog = heading + crab_angle
         speed = np.sqrt(xs[3] ** 2 + xs[4] ** 2)
         return np.array([xs[0], xs[1], speed, cog])
     else:
         heading = xs[2, :]
-        crab_angle = np.zeros(len(xs[2, :]))  # np.arctan2(xs[4, :], xs[3, :])
+        crab_angle = np.arctan2(xs[4, :], xs[3, :])
         cog = heading + crab_angle
         speed = np.sqrt(np.multiply(xs[3, :], xs[3, :]) + np.multiply(xs[4, :], xs[4, :]))
         return np.array([xs[0, :], xs[1, :], speed, cog])

colav_simulator/core/models.py
@@ -502,6 +502,7 @@ class Telemetron(IModel):
         nu_c = mf.Rmtrx(eta[2]).T @ np.array([V_c * np.cos(beta_c), V_c * np.sin(beta_c), 0.0])
         nu_w = mf.Rmtrx(eta[2]).T @ np.array([V_w * np.cos(beta_w), V_w * np.sin(beta_w), 0.0])
         nu_r = nu - nu_c
+        nu_c_dot = np.array([nu[2] * nu_c[1], -nu[2] * nu_c[0], 0.0])
 
         u[0] = mf.sat(u[0], self._params.Fx_limits[0], self._params.Fx_limits[1])
         u[1] = mf.sat(u[1], self._params.Fy_limits[0], self._params.Fy_limits[1])
@@ -515,8 +516,7 @@ class Telemetron(IModel):
 
         ode_fun = np.zeros(6)
         ode_fun[0:3] = mf.Rmtrx(eta[2]) @ nu
-        ode_fun[3:6] = Minv @ (-Cvv - Dvv + u + tau_wind)
-
+        ode_fun[3:6] = Minv @ (-Cvv - Dvv + u + tau_wind) + nu_c_dot
         return ode_fun
 
     def _compute_wind_coefficients(self, gamma_rw: float) -> Tuple[float, float, float]:

colav_simulator/gym/environment.py
@@ -44,7 +44,8 @@ class COLAVEnvironment(gym.Env):
         scenario_config: Optional[sc.ScenarioConfig | pathlib.Path] = None,
         scenario_file_folder: Optional[pathlib.Path] = None,
         reload_map: Optional[bool] = True,
-        rewarder_config: Optional[rw.Config] = None,
+        rewarder_class: Optional[rw.IReward] = rw.Rewarder,
+        rewarder_kwargs: Optional[dict] = {},
         action_type: Optional[str] = None,
         action_sampling_time: Optional[float] = None,
         observation_type: Optional[dict | str] = None,
@@ -68,7 +69,8 @@ class COLAVEnvironment(gym.Env):
             scenario_config (Optional[sc.ScenarioConfig | Path]): Scenario configuration, either config object or path. Defaults to None.
             scenario_file_folder (Optional[list | Path): Folder path to scenario episode files. Defaults to None.
             reload_map (Optional[bool]): Whether to reload the scenario ENC map. Defaults to False. NOTE: Might cause issues with vectorized environments due to race conditions.
-            rewarder_config (Optional[rw.Config]): Rewarder configuration. Defaults to None.
+            rewarder_class (Optional[rw.IReward]): Rewarder class. Defaults to rw.Rewarder.
+            rewarder_kwargs (Optional[dict]): Rewarder keyword arguments. Defaults to {}.
             action_type (Optional[str]): Action type. Defaults to None.
             observation_type (Optional[dict | str]): Observation type. Defaults to None.
             render_mode (Optional[str]): Render mode. Defaults to "human".
@@ -115,8 +117,6 @@ class COLAVEnvironment(gym.Env):
         self.verbose: bool = verbose
         self.current_frame: np.ndarray = np.zeros((1, 1, 3), dtype=np.uint8)
 
-        self.rewarder: rw.Rewarder = rw.Rewarder(env=self, config=rewarder_config)
-
         if self.scenario_file_folder is not None:
             self._load(
                 scenario_file_folder=self.scenario_file_folder,
@@ -148,6 +148,7 @@ class COLAVEnvironment(gym.Env):
         )
         self.dt_action = action_sampling_time
 
+        self.rewarder = rewarder_class(env=self, **rewarder_kwargs)
         self._define_spaces()
 
     def close(self):

colav_simulator/gym/observation.py
@@ -16,7 +16,7 @@
 
 import time
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Tuple, Union
+from typing import TYPE_CHECKING, Dict, Tuple, Union
 
 import colav_simulator.common.image_helper_methods as imghf
 import colav_simulator.common.map_functions as mapf
@@ -32,7 +32,7 @@ import shapely
 import shapely.geometry as sgeo
 from matplotlib import gridspec
 
-Observation = Union[tuple, list, np.ndarray]
+Observation = Union[tuple, list, np.ndarray, Dict[str, np.ndarray]]
 
 if TYPE_CHECKING:
     from colav_simulator.gym.environment import COLAVEnvironment
@@ -394,8 +394,11 @@ class PathRelativeNavigationObservation(ObservationType):
         self.name = "PathRelativeNavigationObservation"
         self.size = 5
         self.define_observation_ranges()
-        self._map_origin = self._map_origin = self._ownship.csog_state[:2]
         self._ktp = guidances.KinematicTrajectoryPlanner()
+        self.create_path()
+
+    def create_path(self) -> None:
+        self._map_origin = self._map_origin = self._ownship.csog_state[:2]
         os_nominal_path = self._ktp.compute_splines(
             waypoints=self._ownship.waypoints - np.array([self._map_origin[0], self._map_origin[1]]).reshape(2, 1),
             speed_plan=self._ownship.speed_plan,
@@ -469,6 +472,8 @@ class PathRelativeNavigationObservation(ObservationType):
         return self._path_linestring.distance(sgeo.Point(p[0], p[1]))
 
     def observe(self) -> Observation:
+        if self.env.time < 0.0001:
+            self.create_path()
         state = self._ownship.state - np.array([self._map_origin[0], self._map_origin[1], 0, 0, 0, 0])
         s = self.get_closest_arclength(state[:2])
         d2path = self.distance_to_path(state[:2])
@@ -601,6 +606,73 @@ class DisturbanceObservation(ObservationType):
             obs[2] = ddata.wind["speed"]
             obs[3] = mf.wrap_angle_diff_to_pmpi(ddata.wind["direction"], os_course)
         return self.normalize(obs)
+
+
+class GroundTruthTrackingObservation(ObservationType):
+    """Observation containing a dict of augmented states [x, y, vx, vy, length, width] and covariances for the dynamic obstacles, non-normalized and non-relative to the own-ship."""
+
+    def __init__(self, env: "COLAVEnvironment") -> None:
+        super().__init__(env)
+        assert self._ownship is not None, "Ownship is not defined"
+        self.max_num_do = 15
+        self.do_info_size = 6  # [x, y, Vx, Vy, length, width]
+        self.name = "GroundTruthTrackingObservation"
+        self.define_observation_ranges()
+
+    def space(self) -> gym.spaces.Space:
+        return gym.spaces.Box(low=-1e12, high=1e12, shape=(self.do_info_size, self.max_num_do), dtype=np.float32)
+
+    def define_observation_ranges(self) -> None:
+        (x_min, y_min, x_max, y_max) = self.env.enc.bbox
+        self.observation_range = {
+            "north": (y_min, y_max),
+            "east": (x_min, x_max),
+            "speed": (-20.0, 20.0),
+            "angles": (-np.pi, np.pi),
+            "length": (0.0, 100.0),
+            "width": (0.0, 100.0),
+        }
+
+    def normalize(self, obs: Observation) -> Observation:
+        norm_obs = np.zeros((self.do_info_size, self.max_num_do), dtype=np.float32)
+        for idx in range(self.max_num_do):
+            norm_obs[:, idx] = np.array(
+                [
+                    mf.linear_map(obs[0, idx], self.observation_range["north"], (-1.0, 1.0)),
+                    mf.linear_map(obs[1, idx], self.observation_range["east"], (-1.0, 1.0)),
+                    mf.linear_map(obs[2, idx], self.observation_range["speed"], (-1.0, 1.0)),
+                    mf.linear_map(obs[3, idx], self.observation_range["speed"], (-1.0, 1.0)),
+                    mf.linear_map(obs[4, idx], self.observation_range["length"], (-1.0, 1.0)),
+                    mf.linear_map(obs[5, idx], self.observation_range["width"], (-1.0, 1.0)),
+                ]
+            )
+        return norm_obs
+
+    def unnormalize(self, obs: Observation) -> Observation:
+        unnorm_obs = np.zeros((self.do_info_size, self.max_num_do), dtype=np.float32)
+        for idx in range(self.max_num_do):
+            unnorm_obs[:, idx] = np.array(
+                [
+                    mf.linear_map(obs[0, idx], (-1.0, 1.0), self.observation_range["north"]),
+                    mf.linear_map(obs[1, idx], (-1.0, 1.0), self.observation_range["east"]),
+                    mf.linear_map(obs[2, idx], (-1.0, 1.0), self.observation_range["speed"]),
+                    mf.linear_map(obs[3, idx], (-1.0, 1.0), self.observation_range["speed"]),
+                    mf.linear_map(obs[4, idx], (-1.0, 1.0), self.observation_range["length"]),
+                    mf.linear_map(obs[5, idx], (-1.0, 1.0), self.observation_range["width"]),
+                ]
+            )
+        return unnorm_obs
+
+    def observe(self) -> Observation:
+        """Get an observation of the environment state."""
+        assert self._ownship is not None, "Ownship is not defined"
+        do_list = self.env.dynamic_obstacles
+        obs = np.zeros((self.do_info_size, self.max_num_do), dtype=np.float32)
+        for idx, do_ship in enumerate(do_list):
+            do_csog_state = do_ship.csog_state
+            do_state = mhm.convert_state_to_vxvy_state(do_csog_state)
+            obs[:6, idx] = np.array([do_state[0], do_state[1], do_state[2], do_state[3], do_ship.length, do_ship.width])
+        return obs
 
 
 class TrackingObservation(ObservationType):
@@ -843,31 +915,24 @@ class DictObservation(ObservationType):
 
         for obs_type in self.observation_types:
             obs_space[obs_type.name] = obs_type.space()
-
         return gym.spaces.Dict(obs_space)
 
     def unnormalize(self, obs: Observation) -> Observation:
         unnormalized_obs = dict()
-
         for obs_type in self.observation_types:
             unnormalized_obs[obs_type.name] = obs_type.unnormalize(obs[obs_type.name])
-
         return unnormalized_obs
 
     def normalize(self, obs: Observation) -> Observation:
         normalized_obs = dict()
-
         for obs_type in self.observation_types:
             normalized_obs[obs_type.name] = obs_type.normalize(obs[obs_type.name])
-
         return normalized_obs
 
     def observe(self) -> Observation:
         obs = dict()
-
         for obs_type in self.observation_types:
             obs[obs_type.name] = obs_type.observe()
-
         return obs
 
 

colav_simulator/gym/reward.py
@@ -9,11 +9,9 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Optional, Tuple, TypeVar
+from typing import TYPE_CHECKING, Optional
 
 import numpy as np
-import seacharts.enc as senc
-from colav_simulator.core.ship import Ship
 from colav_simulator.gym.action import Action
 from colav_simulator.gym.observation import Observation
 
@@ -38,7 +36,7 @@ class ExistenceRewardParams:
 
     def to_dict(self):
         return {"r_exists": self.r_exists}
-    
+
 
 @dataclass
 class CollisionRewardParams:
@@ -57,7 +55,7 @@ class CollisionRewardParams:
 
     def to_dict(self):
         return {"r_collision": self.r_collision}
-    
+
 
 @dataclass
 class GroundingRewardParams:
@@ -204,7 +202,9 @@ class ExistenceRewarder(IReward):
 class DistanceToGoalRewarder(IReward):
     """Reward the agent for getting closer to the goal."""
 
-    def __init__(self, env: "COLAVEnvironment", goal: np.ndarray, params: Optional[DistanceToGoalRewardParams] = None) -> None:
+    def __init__(
+        self, env: "COLAVEnvironment", goal: np.ndarray, params: Optional[DistanceToGoalRewardParams] = None
+    ) -> None:
         """Initializes the reward function.
 
         Args:
@@ -217,7 +217,7 @@ class DistanceToGoalRewarder(IReward):
     def __call__(self, state: Observation, action: Optional[Action] = None, **kwargs) -> float:
         if self.env.ownship._goal_state.size > 0:
             self.goal = self.env.ownship._goal_state[:2]
-        elif self.env.ownship._waypoints.size > 1: 
+        elif self.env.ownship._waypoints.size > 1:
             self.goal = self.env.ownship._waypoints[:, -1]
         else:
             raise ValueError("No goal state or waypoints found")
@@ -241,7 +241,7 @@ class TrajectoryTrackingRewarder(IReward):
 
     def __call__(self, state: Observation, action: Optional[Action] = None, **kwargs) -> float:
         return -float(np.linalg.norm(state[:2] - self.trajectory))
-    
+
 
 class CollisionRewarder(IReward):
     """Reward the agent negatively for colliding."""
@@ -259,9 +259,10 @@ class CollisionRewarder(IReward):
     def __call__(self, state: Observation, action: Optional[Action] = None, **kwargs) -> float:
         """Reward for the current state-action pair. Calls function from simulator class to get collision or not."""
         collision = self.env.simulator.determine_ownship_collision()
-        if collision: 
-            return self.params.r_collision 
-        else: return 0.0
+        if collision:
+            return self.params.r_collision
+        else:
+            return 0.0
 
 
 class GroundingRewarder(IReward):
@@ -280,9 +281,10 @@ class GroundingRewarder(IReward):
     def __call__(self, state: Observation, action: Optional[Action] = None, **kwargs) -> float:
         """Reward for the current state-action pair. Calls function from simulator class to get grounding or not."""
         grounding = self.env.simulator.determine_ownship_grounding()
-        if grounding: 
+        if grounding:
             return self.params.r_grounding
-        else: return 0.0
+        else:
+            return 0.0
 
 
 class GoalReachedRewarder(IReward):
@@ -297,6 +299,7 @@ class GoalReachedRewarder(IReward):
         """
         super().__init__(env)
         self.params = params if params else GoalReachedRewardParams()
+
     def __call__(self, state: Observation, action: Optional[Action] = None, **kwargs) -> float:
         """Reward for the current state-action pair. Calls function from simulator class to get goal reached or not."""
         goal_reached = self.env.simulator.determine_ownship_goal_reached()
@@ -304,10 +307,11 @@ class GoalReachedRewarder(IReward):
             return self.params.r_goal_reached
         return 0.0
 
+
 class Rewarder(IReward):
     """The rewarder class."""
 
-    def __init__(self,  env: "COLAVEnvironment", config: Optional[Config] = None) -> None:
+    def __init__(self, env: "COLAVEnvironment", config: Optional[Config] = None) -> None:
         """Initializes the rewarder.
 
         Args: